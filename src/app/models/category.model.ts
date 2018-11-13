c32b7d0257dcc470a15124b75ef9d6378de3b1f6
Remiee <nemes.agi96@gmail.com>
2018-11-13 18:31:59 +0100

    Refact models

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,9 +1,3 @@
 export class Category {
-  public id: string;
-  public name: string;
-
-  constructor(id: string, name: string) {
-    this.id = id;
-    this.name = name;
-  }
+  constructor(public id: string, public name: string) { }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,15 +1,7 @@
 export class User {
-  public id: string;
-  public firstName: string;
-  public lastName: string;
-  public email: string;
-  public password: string;
-
-  constructor(id: string, firstName: string, lastName: string, email: string, password: string) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-  }
+  constructor(public id: string,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public password: string) { }
 }
